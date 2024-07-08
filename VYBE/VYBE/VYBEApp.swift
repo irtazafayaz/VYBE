01f7215511eeee6e5ff0f70e53a4c2bf804a71ba
Hamza Alam Hashmi <hamzahalam2001@gmail.com>
2024-07-09 02:17:38 +0500

    Initial Commit

---
+++ VYBE/VYBE/VYBEApp.swift
@@ -0,0 +1,17 @@
+//
+//  VYBEApp.swift
+//  VYBE
+//
+//  Created by Hamza Hashmi on 09/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct VYBEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
